24ef18c50abc226b804456c6e39836d3f2a6e8ac
M45t3rm1nd <M45t3rm1nd@169.254.199.249>
2017-12-23 13:33:23 +0530

    committing changes

--- AutomationDemo/src/test/java/com/bitwiseglobal/qa/AmazonHomeTest.java
+++ AutomationDemo/src/test/java/com/bitwiseglobal/qa/AmazonHomeTest.java
@@ -1,8 +1,6 @@
 package com.bitwiseglobal.qa;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,36 +10,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class AmazonHomeTest {
 	WebDriver driver;
-	
+
 	@Before
 	public void initializeDriver() {
 		driver = new ChromeDriver();
 		driver.get("https://www.amazon.com");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void validateSearchFunctionality() throws InterruptedException {
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("heater\r\n");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Lasko 751320 Ceramic Tower Heater with Remote Control\r\n");
 		Thread.sleep(10000);
 		List<WebElement> productNames = driver.findElement(By.id("s-results-list-atf")).findElements(By.tagName("h2"));
-		System.out.println("Current URL "+driver.getCurrentUrl());		
-		System.out.println("List " + productNames);		
-		System.out.println("\n\n\nproducts Avl. "+productNames.size());
-		System.out.println("===================================");
-		productNames.stream().forEach(e -> System.out.println(e.getText()));
-		List<WebElement> elementsWithKeyword = productNames.stream().filter(e -> e.getText().toLowerCase().trim().contains("heater")).collect(Collectors.toList());
-		System.out.println("\n\n\n\nElements with keyword \"heater\" = "+elementsWithKeyword.size());
-		System.out.println("===================================");
-		elementsWithKeyword.stream().forEach(e -> System.out.println(e.getText()));
-		Assert.assertTrue(productNames.size()==elementsWithKeyword.size());
+		String addedProduct = productNames.get(0).getText();
+		productNames.get(0).click();
+		Thread.sleep(5000);
+		driver.findElement(By.id("add-to-cart-button")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.id("nav-cart-count")).click();
+		Thread.sleep(5000);
+		String cartAddedProductValue = driver.findElement(By.xpath("//span[@class='a-size-medium sc-product-title a-text-bold']")).getText();
+		System.out.println(cartAddedProductValue);
+		System.out.println(addedProduct);
+		Assert.assertEquals(cartAddedProductValue.trim(), addedProduct.trim());
+		Assert.assertEquals("1", driver.findElement(By.id("nav-cart-count")).getText().trim());
 	}
-	
+
 	@After
 	public void destroyDriver() {
-	driver.quit();
+		driver.quit();
 	}
 }
